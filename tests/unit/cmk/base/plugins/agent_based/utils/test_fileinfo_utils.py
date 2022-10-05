4bc745aaaf89c763707be57a050306e46c4708a5
Dirk Mayer <71777632+dirkhub@users.noreply.github.com>
2022-10-05 18:00:10 +0200

    deliberate fault in unit test

--- tests/unit/cmk/base/plugins/agent_based/utils/test_fileinfo_utils.py
+++ tests/unit/cmk/base/plugins/agent_based/utils/test_fileinfo_utils.py
@@ -388,7 +388,7 @@ def test__filename_matches(  # type:ignore[no-untyped-def]
             ),
             [
                 Result(state=State.OK, notice="Include patterns: ~my_folder/*.dat"),
-                Result(state=State.OK, summary="Count: 0"),
+                Result(state=State.OK, summary="Count: 1"),
                 Metric("count", 0),
                 Result(state=State.OK, summary="Size: 0 B"),
                 Metric("size", 0),
